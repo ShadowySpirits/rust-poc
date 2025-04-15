7530fc2148f86f432d5c99b78743a430fe685cee
SSpirits <admin@lv5.moe>
2025-04-15 11:31:08 +0800

    feat: listen tcp and tls at the same time

--- src/mqtt_gateway/mod.rs
+++ src/mqtt_gateway/mod.rs
@@ -8,7 +8,7 @@ use ntex::tls::rustls::{PeerCert, TlsAcceptor, TlsServerFilter};
 use ntex::util::Ready;
 use ntex::{chain_factory, fn_service};
 use ntex_io::{Io, Layer};
-use ntex_mqtt::{MqttError, MqttServer, v3, v5};
+use ntex_mqtt::{v3, v5, MqttError, MqttServer};
 use rustls::server::WebPkiClientVerifier;
 use rustls::{RootCertStore, ServerConfig};
 use std::fs::File;
@@ -24,7 +24,31 @@ mod middleware;
 mod session;
 mod upstream;
 
-async fn serve() -> std::io::Result<()> {
+async fn listen_tcp() -> std::io::Result<()> {
+    ntex::server::Server::build()
+        .bind("mqtt-gateway", "0.0.0.0:1884", move |_| {
+            let mqtt_v3_server = v3::MqttServer::new(connect_v3)
+                .control(control_factory_v3())
+                .publish(publish_factory_v3())
+                .middleware(RequestLogger)
+                // .middleware(fn_pub_ack_factory_v3())
+                // .middleware(fn_handle_packet_id())
+                // .middleware(fn_auth)
+                .finish();
+
+            let mqtt_v5_server = v5::MqttServer::new(connect_v5)
+                .control(control_factory_v5())
+                .publish(publish_factory_v5())
+                .finish();
+
+            MqttServer::new().v3(mqtt_v3_server).v5(mqtt_v5_server)
+        })?
+        .workers(1)
+        .run()
+        .await
+}
+
+async fn listen_tls() -> std::io::Result<()> {
     let cert_file = &mut BufReader::new(File::open("resources/server.chain.crt")?);
     let key_file = &mut BufReader::new(File::open("resources/server.pkcs8.key")?);
     let keys = rustls_pemfile::private_key(key_file)?.unwrap();
@@ -44,7 +68,7 @@ async fn serve() -> std::io::Result<()> {
     );
 
     ntex::server::Server::build()
-        .bind("mqtt-gateway", "0.0.0.0:1884", move |_| {
+        .bind("mqtt-gateway", "0.0.0.0:1885", move |_| {
             chain_factory(TlsAcceptor::new(tls_config.clone()))
                 .map_err(|err| {
                     println!("Tls handshake failed: {}", err);
@@ -54,9 +78,13 @@ async fn serve() -> std::io::Result<()> {
                     // Handle peer certificate
                     println!(
                         "Incoming tls connection: peer certificate: {:?}",
-                        io.query::<PeerCert>()
-                            .as_ref()
-                            .map(|cert: &PeerCert| X509Certificate::from_der(&cert.0).unwrap().1.subject().to_string())
+                        io.query::<PeerCert>().as_ref().map(|cert: &PeerCert| {
+                            X509Certificate::from_der(&cert.0)
+                                .unwrap()
+                                .1
+                                .subject()
+                                .to_string()
+                        })
                     );
                     Ready::Ok(io)
                 }))
@@ -77,8 +105,8 @@ async fn serve() -> std::io::Result<()> {
 
                     MqttServer::new().v3(mqtt_v3_server).v5(mqtt_v5_server)
                 })
-                // TODO: add error handler
-                // .then(service_error_handler)
+            // TODO: add error handler
+            // .then(service_error_handler)
         })?
         .workers(1)
         .run()
@@ -87,5 +115,7 @@ async fn serve() -> std::io::Result<()> {
 
 #[ntex::main]
 async fn main() {
-    serve().await.unwrap()
+    let tcp_handle = ntex::rt::spawn(listen_tcp());
+    let tls_handle = ntex::rt::spawn(listen_tls());
+    let _ = tokio::join!(tcp_handle, tls_handle);
 }
